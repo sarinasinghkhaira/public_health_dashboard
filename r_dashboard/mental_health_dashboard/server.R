5278e0c5192d224155bb3026fb841831a4f7514e
ganderson1 <graeme.anderson81@gmail.com>
2021-01-26 14:14:29 +0000

    server changes

--- r_dashboard/mental_health_dashboard/server.R
+++ r_dashboard/mental_health_dashboard/server.R
@@ -13,11 +13,5 @@ library(shinydashboard)
 # Define server logic required to draw a histogram
 
 server <- function(input, output) {
-    set.seed(122)
-    histdata <- rnorm(500)
     
-    output$plot1 <- renderPlot({
-        data <- histdata[seq_len(input$slider)]
-        hist(data)
-    })
 }
